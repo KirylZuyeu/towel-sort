37dad495d1cddc8fa085eb9dd606a41aa446769e
KirylZuyeu <kirylzuyeuimh@gmail.com>
2020-03-08 21:17:40 +0300

    init: add decision of Towel Sort Task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix == null) return [];
+    let sortArr = [];
+    for (let i = 0; i < matrix.length; i++){
+        if (i % 2 == 0) {
+            sortArr.push(matrix[i]);
+        } else {
+            sortArr.push(matrix[i].reverse());
+        }
+}
+return [].concat(...sortArr);
 }
